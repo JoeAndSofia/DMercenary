7c696f12256e6f6713c1c66f996dd5005531807f
joe.wang <joe.wang@imaginato.com>
2015-09-06 17:01:46 +0800

    20150906-commit0009

---
+++ JavaRelates/JavaRelates/src/study007_DateAndTime/DateTest.java
@@ -0,0 +1,48 @@
+package study007_DateAndTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTest {
+	private static final String[] MONTH_STR = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+			
+	public static void main(String[] args){
+//		test001(new Date());
+		test002(new Date());
+	}
+	
+	public static void test001(Date date){
+		System.out.println(date.toGMTString());
+		System.out.println(date.toLocaleString());
+		System.out.println(date.toString());
+		
+		System.out.println(date.getYear()+1900);
+		System.out.println(date.getMonth());
+		System.out.println(date.getDate());
+		System.out.println("******");		
+		System.out.println(date.getHours());
+		System.out.println(date.getMinutes());
+		System.out.println(date.getSeconds());
+		System.out.println("******");
+		System.out.println(date.getDay());
+		String y = String.valueOf(date.getYear());
+		String d = String.valueOf(date.getDate());
+		String M = MONTH_STR[date.getMonth()];
+//		String H = String.valueOf(dat)
+		
+		StringBuilder sb = new StringBuilder(19);
+		sb.append(date.getDate()+" ").append(MONTH_STR[date.getMonth()]+" ").append(date.getYear()%100+", ");
+	}
+	
+	public static void test002(Date d){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yy, hh:mm a");
+		String str = sdf.format(d);
+		if(str.substring(17).equalsIgnoreCase("pm")){
+			str = str.substring(0, 17) + "pm";
+		}else{
+			str = str.substring(0,17) + "am";
+		}
+		System.out.println(str);
+	}
+}
---
+++ JavaRelates/JavaRelates/src/study008_swing/TestPanel.java
@@ -0,0 +1,12 @@
+package study008_swing;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TestPanel{
+	
+}
+
+class DMercenary extends JFrame{
+	private JPanel sandbox = new JPanel();
+}
